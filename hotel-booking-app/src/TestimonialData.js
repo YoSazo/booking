2b4407de6f9a34daaa8833a9c560385e3cfe9b52
sazo <salahsalad100@gmail.com>
2025-10-22 19:23:38 -0500

    changes

--- hotel-booking-app/src/TestimonialData.js
+++ hotel-booking-app/src/TestimonialData.js
@@ -5,7 +5,7 @@ export const testimonials = [
     id: 1,
     name: "Sarah Johnson",
     nights: 50,
-    videoUrl: "https://player.cloudinary.com/embed/?cloud_name=dkmr3h5jb&public_id=Hello_-_Cutest_3_Second_Video_Ever_-_gardea23_1080p_h264_eyhlj4&profile=cld-default", // Store in public/testimonials/
+    videoUrl: "https://res.cloudinary.com/dkmr3h5jb/video/upload/v1761178673/Hello_-_Cutest_3_Second_Video_Ever_-_gardea23_1080p_h264_eyhlj4.mp4", // Store in public/testimonials/
     thumbnailUrl: "/testimonials/dogtn.jpg",
     confirmationImageUrl: "/testimonials/dogconfirmation.png",
   }
