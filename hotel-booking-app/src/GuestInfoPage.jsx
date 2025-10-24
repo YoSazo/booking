2464cdf00a5a8bc8a1a63d83217f3c4f31d10ba9
sazo <salahsalad100@gmail.com>
2025-10-24 15:16:59 -0500

    changes

--- hotel-booking-app/src/GuestInfoPage.jsx
+++ hotel-booking-app/src/GuestInfoPage.jsx
@@ -601,7 +601,9 @@ useEffect(() => {
                     </div>
 
                     <div className="payment-wrapper" style={{ display: currentStep === 3 ? 'block' : 'none' }}>
-                        
+                        <div className="stripe-badge-container">
+        <img src="/powerstripe.svg" alt="Powered by Stripe" className="stripe-badge" />
+    </div>
                         <div className="secure-checkout-badge">
     <img src="/lock.svg" alt="Secure Checkout" className="lock-icon" />
     <span>Guaranteed safe and secure Checkout</span>
