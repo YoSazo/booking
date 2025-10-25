317e988b2e114a46817c40f6f15c0dfc1b956fd0
sazo <salahsalad100@gmail.com>
2025-10-24 19:31:45 -0500

    changes

--- hotel-booking-app/src/TestimonialData.js
+++ hotel-booking-app/src/TestimonialData.js
@@ -3,10 +3,10 @@
 export const testimonials = [
   {
     id: 1,
-    name: "Sarah Johnson",
-    nights: 50,
-    videoUrl: "https://res.cloudinary.com/dkmr3h5jb/video/upload/v1761178673/Hello_-_Cutest_3_Second_Video_Ever_-_gardea23_1080p_h264_eyhlj4.mp4", // Store in public/testimonials/
-    thumbnailUrl: "/testimonials/dogtn.jpg",
-    confirmationImageUrl: "/testimonials/dogconfirmation.png",
+    name: "Ve grranger",
+    nights: 33,
+    videoUrl: "https://res.cloudinary.com/dkmr3h5jb/video/upload/v1761352050/1024_3_hzsilz.mp4", // Store in public/testimonials/
+    thumbnailUrl: "/testimonials/thumbnail-ve.png",
+    confirmationImageUrl: "/testimonials/confirmation-ve.png",
   }
 ];
