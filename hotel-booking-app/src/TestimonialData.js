71d0273cd76b252e7547290080b1b4b3997f028e
sazo <salahsalad100@gmail.com>
2025-10-25 20:46:30 -0500

    changes

--- hotel-booking-app/src/TestimonialData.js
+++ hotel-booking-app/src/TestimonialData.js
@@ -1,24 +1,22 @@
 // hotel-booking-app/src/TestimonialData.js
 
 export const testimonials = [
-
   {
     id: 1,
-    name: "Brent Williams",
-    nights: 56,
-    videoUrl: "https://res.cloudinary.com/dkmr3h5jb/video/upload/v1761353644/1024_6_vylknz.mp4", // Store in public/testimonials/
-    thumbnailUrl: "/testimonials/thumbnail-brent.png",
-    confirmationImageUrl: "/testimonials/confirmation-brent.png",
-  },
-
-
-  {
-    id: 2,
     name: "Ve Grranger",
     nights: 33,
     videoUrl: "https://res.cloudinary.com/dkmr3h5jb/video/upload/v1761352050/1024_3_hzsilz.mp4", // Store in public/testimonials/
     thumbnailUrl: "/testimonials/thumbnail-ve.png",
     confirmationImageUrl: "/testimonials/confirmation-ve.png",
+  },
+
+  {
+    id: 2,
+    name: "Brent Williams",
+    nights: 56,
+    videoUrl: "https://res.cloudinary.com/dkmr3h5jb/video/upload/v1761353644/1024_6_vylknz.mp4", // Store in public/testimonials/
+    thumbnailUrl: "/testimonials/thumbnail-brent.png",
+    confirmationImageUrl: "/testimonials/confirmation-brent.png",
   },
 
   {
