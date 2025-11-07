15cde2626e8422583288ec315b877dd65007070f
sazo <salahsalad100@gmail.com>
2025-11-07 15:26:17 -0600

    changes

--- hotel-booking-app/src/TestimonialData.js
+++ hotel-booking-app/src/TestimonialData.js
@@ -5,7 +5,7 @@ export const testimonials = [
     id: 1,
     name: "Kenneth Griffin",
     nights: 369,
-    videoUrl: "https://res.cloudinary.com/dkmr3h5jb/video/upload/v1762532361/IMG_7553_njkbmt.mov", // Store in public/testimonials/
+    videoUrl: "https://res.cloudinary.com/dkmr3h5jb/video/upload/v1762550747/1107_1_mnsums.mp4", // Store in public/testimonials/
     thumbnailUrl: "/testimonials/thumbnail-kenneth.png",
     confirmationImageUrl: "/testimonials/confirmation-kenneth.png",
   }
